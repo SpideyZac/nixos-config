850577c63e306f7b1db0bd6c4c449ac1e204d534
SpideyZac <zacmlesser-7@outlook.com>
2025-08-02 01:49:23 -0700

    feat: add gh & remove default git config

--- home.nix
+++ home.nix
@@ -8,11 +8,5 @@
     inputs.zen-browser.packages."${system}".default
   ];
   
-  programs.git = {
-    enable = true;
-    userName = "SpideyZac";
-    userEmail = "zacmlesser-7@outlook.com";
-  };
-  
   home.stateVersion = "25.05";
 }
