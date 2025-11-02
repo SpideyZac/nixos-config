211058f6a684b24fc055d584f881e654fbec12c6
spideyzac <zacmlesser-7@outlook.com>
2025-11-02 10:32:16 -0800

    fix: add git config

--- zacml-home.nix
+++ zacml-home.nix
@@ -4,5 +4,10 @@
   home.username = "zacml";
   home.homeDirectory = "/home/zacml";
 
+  programs.git = {
+    userName = "SpideyZac";
+    userEmail = "zacmlesser-7@outlook.com";
+  };
+
   home.stateVersion = "25.05";
 }
